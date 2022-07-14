21f0143355ba4db30580486f9bbc0221b62df68f
Patrick Maul <maulpatrick1996@gmail.com>
2022-07-15 00:46:07 +0200

    fix: routing

--- src/components/core/AppBar.vue
+++ src/components/core/AppBar.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 header.navbar.navbar-expand-lg.bg-light
   .container-fluid
-    a.navbar-brand(href="#")
+    a.navbar-brand(href="/")
       img(src="../../assets/logos/logo_dark.svg", alt="Logo", height="60px", width="100px")
+
     button.navbar-toggler.rounded-pill.border-0.position-relative.me-2(type="button")
       i.bi.bi-list.position-absolute.top-50.start-50.translate-middle
 </template>
